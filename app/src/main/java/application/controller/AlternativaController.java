f422c2f23ebf44b10c6b7544d53b25d704957d66
Gabriel Paião <costapaiao@hotmail.com>
2023-11-28 19:53:40 +0000

    .

--- app/src/main/java/application/controller/AlternativaController.java
+++ app/src/main/java/application/controller/AlternativaController.java
@@ -7,12 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import application.repository.AlternativaRepository;
 import org.springframework.ui.Model;
 
-import application.model.Alternativas;
-import application.repository.AlternativaRepository;
-import application.repository.QuestaoRepository;
-
+import application.model.Alternativa;
 
 @Controller
 @RequestMapping("/alternativa")
@@ -21,80 +20,61 @@ public class AlternativaController {
     @Autowired
     private AlternativaRepository alternativaRepository;
 
-    @Autowired
-    private QuestaoRepository questaoRepository;
-
     @RequestMapping("/list")
     public String list(Model ui) {
         ui.addAttribute("alternativas", alternativaRepository.findAll());
-        return "alternativa/list";
+        return "/alternativa/list";
     }
 
     @RequestMapping("/insert")
-    public String insert(Model ui) {
-        ui.addAttribute("questoes", questaoRepository.findAll());
-        return "alternativa/insert";
+    public String insert() {
+        return "/alternativa/insert";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public String insert(
-            @RequestParam("texto") String texto,
-            @RequestParam(name = "correta", defaultValue = "true") Boolean isCorreta,
-            @RequestParam(name = "falso", defaultValue = "false") Boolean isFalso)
-    {
-        Alternativas alternativa = new Alternativas();
+    public String insert(@RequestParam String texto,
+            @RequestParam(value = "correta", required = false, defaultValue = "false") boolean correta, Model ui) {
+        Alternativa alternativa = new Alternativa();
         alternativa.setTexto(texto);
-        alternativa.setCorreta(isCorreta);
-        alternativa.setFalso(isFalso);
+        alternativa.setCorreta(correta);
         alternativaRepository.save(alternativa);
         return "redirect:/alternativa/list";
     }
 
-
     @RequestMapping("/update")
-    public String update(@RequestParam("id") Long id, Model ui) {
-        Optional<Alternativas> alternativa = alternativaRepository.findById(id);
-        if (alternativa.isPresent())    {
-            ui.addAttribute("alternativa", alternativa.get());
-            ui.addAttribute("questoes", questaoRepository.findAll());
-            return "alternativa/update";
-        }
-        return "redirect:/alternativa/list";
+    public String update(@RequestParam Long id, Model ui) {
+        ui.addAttribute("alternativa", alternativaRepository.findById(id));
+        return "/alternativa/update";
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(
-            @RequestParam("id") Long id,
-            @RequestParam("texto") String texto,
-            @RequestParam(name = "correta", defaultValue = "true") Boolean isCorreta,
-            @RequestParam(name = "falso", defaultValue = "false") Boolean isFalso)
-    {
-        Optional<Alternativas> alternativa = alternativaRepository.findById(id);
+    public String update(@RequestParam Long id, @RequestParam String texto,
+            @RequestParam(value = "correta", required = false, defaultValue = "false") boolean correta, Model ui) {
+        Optional<Alternativa> alternativa = alternativaRepository.findById(id);
 
-        if(alternativa.isPresent()) {
+        if (alternativa.isPresent()) {
             alternativa.get().setTexto(texto);
-            alternativa.get().setCorreta(isCorreta);
-            alternativa.get().setFalso(isFalso);
+            alternativa.get().setCorreta(correta);
             alternativaRepository.save(alternativa.get());
         }
         return "redirect:/alternativa/list";
     }
 
-
     @RequestMapping("/delete")
-    public String delete(@RequestParam("id") Long id, Model ui) {
-        Optional<Alternativas> alternativa = alternativaRepository.findById(id);
-        if(alternativa.isPresent()) {
+    public String delete(@RequestParam Long id, Model ui) {
+        Optional<Alternativa> alternativa = alternativaRepository.findById(id);
+        if (alternativa.isPresent()) {
             ui.addAttribute("alternativa", alternativa.get());
-            return "alternativa/delete";
+            return "/alternativa/delete";
         }
         return "redirect:/alternativa/list";
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String delete(@RequestParam("id") Long id) {
+    public String delete(@RequestParam Long id) {
         alternativaRepository.deleteById(id);
         return "redirect:/alternativa/list";
     }
+    
 
 }
--- app/src/main/java/application/controller/QuestaoController.java
+++ app/src/main/java/application/controller/QuestaoController.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,19 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import application.repository.QuestaoRepository;
+import application.model.Questao;
 import application.repository.AlternativaRepository;
-
+import application.repository.QuestaoRepository;
 import org.springframework.ui.Model;
-
-import application.model.Questoes;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/questao")
 public class QuestaoController {
-
+    
     @Autowired
     private QuestaoRepository questaoRepository;
 
@@ -29,65 +26,57 @@ public class QuestaoController {
     @RequestMapping("/list")
     public String list(Model ui) {
         ui.addAttribute("questoes", questaoRepository.findAll());
-        return "questao/list";
+        return "/questao/list";
     }
 
     @RequestMapping("/insert")
-    public String insert(Model ui) {
-        ui.addAttribute("alternativas", alternativaRepository.findAll());
-        return "questao/insert";
+    public String insert() {
+        return "/questao/insert";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public String insert(@RequestParam("enunciado") String enunciado)
-    {
-        Questoes questao = new Questoes();
+    public String insert(@RequestParam String enunciado, Model ui) {
+        Questao questao = new Questao();
         questao.setEnunciado(enunciado);
         questaoRepository.save(questao);
-        System.out.println("Enunciado: " + enunciado);
-        return "redirect:/questao/list";
-    }
-
-    @RequestMapping("/update")
-    public String update(@RequestParam("id") Long id, Model ui) {
-        Optional<Questoes> questao = questaoRepository.findById(id);
-
-        if (questao.isPresent()) {
-            ui.addAttribute("questao", questao.get());
-            ui.addAttribute("alternativas", alternativaRepository.findAll());
-            return "questao/update";
-        }
-
-        return "redirect:/questao/list";
-    }
-
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(@RequestParam("id") Long id, @RequestParam("enunciado") String enunciado) {
-        Optional<Questoes> questao = questaoRepository.findById(id);
-
-        if(questao.isPresent()) {
-            questao.get().setEnunciado(enunciado);
-            questaoRepository.save(questao.get());
-        }
-
         return "redirect:/questao/list";
     }
 
     @RequestMapping("/delete")
-    public String delete(@RequestParam("id") Long id, Model ui) {
-        Optional<Questoes> questao = questaoRepository.findById(id);
-
-        if(questao.isPresent()) {
+    public String delete(@RequestParam Long id, Model ui) {
+        Optional<Questao> questao = questaoRepository.findById(id);
+        if (questao.isPresent()) {
             ui.addAttribute("questao", questao.get());
-            return "questao/delete";
+            return "/questao/delete";
         }
 
         return "redirect:/questao/list";
     }
 
-    @RequestMapping(value="delete", method = RequestMethod.POST)
-    public String delete(@RequestParam("id") Long id) {
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam Long id) {
         questaoRepository.deleteById(id);
+        return "redirect:/questao/list";
+    }
+
+    @RequestMapping("/update")
+    public String update(@RequestParam Long id, Model ui) {
+        System.out.println(id);
+        ui.addAttribute("quest", questaoRepository.findById(id).get());
+
+        return "/questao/update";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@RequestParam Long id, @RequestParam String enunciado, Model ui) {
+        Optional<Questao> questao = questaoRepository.findById(id);
+
+        if (questao.isPresent()) {
+            questao.get().setEnunciado(enunciado);
+            questaoRepository.save(questao.get());
+
+        }
+
         return "redirect:/questao/list";
     }
 }
